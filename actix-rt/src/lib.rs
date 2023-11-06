6693abbacd4261349b0499ba8787c2df2f15cf11
imededin <iati@sambaclab.com>
2023-11-06 11:06:13 +0100

    modify lib

--- actix-rt/src/lib.rs
+++ actix-rt/src/lib.rs
@@ -58,100 +58,100 @@ pub use actix_macros::main;
 #[cfg(feature = "macros")]
 pub use actix_macros::test;
 
-mod arbiter;
-mod runtime;
-mod system;
+// mod arbiter;
+// mod runtime;
+// mod system;
 
 pub use tokio::pin;
 use tokio::task::JoinHandle;
 
-pub use self::{
-    arbiter::{Arbiter, ArbiterHandle},
-    runtime::Runtime,
-    system::{System, SystemRunner},
-};
+// pub use self::{
+//     arbiter::{Arbiter, ArbiterHandle},
+//     runtime::Runtime,
+//     system::{System, SystemRunner},
+// };
 
-pub mod signal {
-    //! Asynchronous signal handling (Tokio re-exports).
+// pub mod signal {
+//     //! Asynchronous signal handling (Tokio re-exports).
 
-    #[cfg(unix)]
-    pub mod unix {
-        //! Unix specific signals (Tokio re-exports).
-        pub use tokio::signal::unix::*;
-    }
-    pub use tokio::signal::ctrl_c;
-}
+//     #[cfg(unix)]
+//     pub mod unix {
+//         //! Unix specific signals (Tokio re-exports).
+//         pub use tokio::signal::unix::*;
+//     }
+//     pub use tokio::signal::ctrl_c;
+// }
 
-pub mod net {
-    //! TCP/UDP/Unix bindings (mostly Tokio re-exports).
+// pub mod net {
+//     //! TCP/UDP/Unix bindings (mostly Tokio re-exports).
 
-    use std::{
-        future::Future,
-        io,
-        task::{Context, Poll},
-    };
+//     use std::{
+//         future::Future,
+//         io,
+//         task::{Context, Poll},
+//     };
 
-    use tokio::io::{AsyncRead, AsyncWrite, Interest};
-    #[cfg(unix)]
-    pub use tokio::net::{UnixDatagram, UnixListener, UnixStream};
-    pub use tokio::{
-        io::Ready,
-        net::{TcpListener, TcpSocket, TcpStream, UdpSocket},
-    };
+//     use tokio::io::{AsyncRead, AsyncWrite, Interest};
+//     #[cfg(unix)]
+//     pub use tokio::net::{UnixDatagram, UnixListener, UnixStream};
+//     pub use tokio::{
+//         io::Ready,
+//         net::{TcpListener, TcpSocket, TcpStream, UdpSocket},
+//     };
 
-    /// Extension trait over async read+write types that can also signal readiness.
-    #[doc(hidden)]
-    pub trait ActixStream: AsyncRead + AsyncWrite + Unpin {
-        /// Poll stream and check read readiness of Self.
-        ///
-        /// See [tokio::net::TcpStream::poll_read_ready] for detail on intended use.
-        fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>>;
+//     /// Extension trait over async read+write types that can also signal readiness.
+//     #[doc(hidden)]
+//     pub trait ActixStream: AsyncRead + AsyncWrite + Unpin {
+//         /// Poll stream and check read readiness of Self.
+//         ///
+//         /// See [tokio::net::TcpStream::poll_read_ready] for detail on intended use.
+//         fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>>;
 
-        /// Poll stream and check write readiness of Self.
-        ///
-        /// See [tokio::net::TcpStream::poll_write_ready] for detail on intended use.
-        fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>>;
-    }
+//         /// Poll stream and check write readiness of Self.
+//         ///
+//         /// See [tokio::net::TcpStream::poll_write_ready] for detail on intended use.
+//         fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>>;
+//     }
 
-    impl ActixStream for TcpStream {
-        fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
-            let ready = self.ready(Interest::READABLE);
-            tokio::pin!(ready);
-            ready.poll(cx)
-        }
+//     impl ActixStream for TcpStream {
+//         fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
+//             let ready = self.ready(Interest::READABLE);
+//             tokio::pin!(ready);
+//             ready.poll(cx)
+//         }
 
-        fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
-            let ready = self.ready(Interest::WRITABLE);
-            tokio::pin!(ready);
-            ready.poll(cx)
-        }
-    }
+//         fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
+//             let ready = self.ready(Interest::WRITABLE);
+//             tokio::pin!(ready);
+//             ready.poll(cx)
+//         }
+//     }
 
-    #[cfg(unix)]
-    impl ActixStream for UnixStream {
-        fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
-            let ready = self.ready(Interest::READABLE);
-            tokio::pin!(ready);
-            ready.poll(cx)
-        }
+//     #[cfg(unix)]
+//     impl ActixStream for UnixStream {
+//         fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
+//             let ready = self.ready(Interest::READABLE);
+//             tokio::pin!(ready);
+//             ready.poll(cx)
+//         }
 
-        fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
-            let ready = self.ready(Interest::WRITABLE);
-            tokio::pin!(ready);
-            ready.poll(cx)
-        }
-    }
+//         fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
+//             let ready = self.ready(Interest::WRITABLE);
+//             tokio::pin!(ready);
+//             ready.poll(cx)
+//         }
+//     }
 
-    impl<Io: ActixStream + ?Sized> ActixStream for Box<Io> {
-        fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
-            (**self).poll_read_ready(cx)
-        }
+//     impl<Io: ActixStream + ?Sized> ActixStream for Box<Io> {
+//         fn poll_read_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
+//             (**self).poll_read_ready(cx)
+//         }
 
-        fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
-            (**self).poll_write_ready(cx)
-        }
-    }
-}
+//         fn poll_write_ready(&self, cx: &mut Context<'_>) -> Poll<io::Result<Ready>> {
+//             (**self).poll_write_ready(cx)
+//         }
+//     }
+// }
 
 pub mod time {
     //! Utilities for tracking time (Tokio re-exports).
@@ -167,36 +167,36 @@ pub mod task {
     pub use tokio::task::{spawn_blocking, yield_now, JoinError, JoinHandle};
 }
 
-/// Spawns a future on the current thread as a new task.
-///
-/// If not immediately awaited, the task can be cancelled using [`JoinHandle::abort`].
-///
-/// The provided future is spawned as a new task; therefore, panics are caught.
-///
-/// # Panics
-/// Panics if Actix system is not running.
-///
-/// # Examples
-/// ```
-/// # use std::time::Duration;
-/// # actix_rt::Runtime::new().unwrap().block_on(async {
-/// // task resolves successfully
-/// assert_eq!(actix_rt::spawn(async { 1 }).await.unwrap(), 1);
-///
-/// // task panics
-/// assert!(actix_rt::spawn(async {
-///     panic!("panic is caught at task boundary");
-/// })
-/// .await
-/// .unwrap_err()
-/// .is_panic());
-///
-/// // task is cancelled before completion
-/// let handle = actix_rt::spawn(actix_rt::time::sleep(Duration::from_secs(100)));
-/// handle.abort();
-/// assert!(handle.await.unwrap_err().is_cancelled());
-/// # });
-/// ```
+// /// Spawns a future on the current thread as a new task.
+// ///
+// /// If not immediately awaited, the task can be cancelled using [`JoinHandle::abort`].
+// ///
+// /// The provided future is spawned as a new task; therefore, panics are caught.
+// ///
+// /// # Panics
+// /// Panics if Actix system is not running.
+// ///
+// /// # Examples
+// /// ```
+// /// # use std::time::Duration;
+// /// # actix_rt::Runtime::new().unwrap().block_on(async {
+// /// // task resolves successfully
+// /// assert_eq!(actix_rt::spawn(async { 1 }).await.unwrap(), 1);
+// ///
+// /// // task panics
+// /// assert!(actix_rt::spawn(async {
+// ///     panic!("panic is caught at task boundary");
+// /// })
+// /// .await
+// /// .unwrap_err()
+// /// .is_panic());
+// ///
+// /// // task is cancelled before completion
+// /// let handle = actix_rt::spawn(actix_rt::time::sleep(Duration::from_secs(100)));
+// /// handle.abort();
+// /// assert!(handle.await.unwrap_err().is_cancelled());
+// /// # });
+// /// ```
 #[track_caller]
 #[inline]
 pub fn spawn<Fut>(f: Fut) -> JoinHandle<Fut::Output>
